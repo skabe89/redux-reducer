2c535e9dc3b09cf48356cc1045f59e7ef1b9d14f
Sean <skabe89@gmail.com>
2021-03-25 09:39:23 -0400

    commit tree 9989c6e749302dd254f1be795fd171b586396559

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action){
+
+  switch(action.type, action.friend){
+
+    
+    case "ADD_FRIEND":
+      const newState = {...state, friends: [...state.friends, action.friend]}
+      return newState
+      
+
+    // case "REMOVE_FRIEND":
+
+  }
+
 }
+
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
-export function managePresents(state, action){
+export function managePresents(state = {numberOfPresents: 0} , action){
+
+  switch(action.type) {
+    case "INCREASE":
+      const newState = {...state}
+      newState.numberOfPresents = state.numberOfPresents + 1
+      return newState
+
+    default:
+      return state
+  }
+  
 }
